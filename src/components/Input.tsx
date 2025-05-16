c9a0c0a8cc55249be847411e97ab728ea51cd8d0
F-mM7 <fmolljazz@gmail.com>
2025-05-16 19:31:10 +0900

    refactor: Inputコンポーネントの型定義を明示化し、構文を整理

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,25 @@
-function Input({
-  label,
-  value,
-  setValue,
-  min = 1,
-  max = 99,
-}: {
+import React from "react";
+
+type InputProps = {
   label: string;
   value: number;
   setValue: (v: number) => void;
   min?: number;
   max?: number;
-}) {
-  return (
-    <label>
-      {label} : {value}
-      <button onClick={() => setValue(Math.min(max, value + 1))}>＋</button>
-      <button onClick={() => setValue(Math.max(min, value - 1))}>−</button>
-    </label>
-  );
-}
+};
+
+const Input: React.FC<InputProps> = ({
+  label,
+  value,
+  setValue,
+  min = 1,
+  max = 99,
+}) => (
+  <label>
+    {label} : {value}
+    <button onClick={() => setValue(Math.min(max, value + 1))}>＋</button>
+    <button onClick={() => setValue(Math.max(min, value - 1))}>−</button>
+  </label>
+);
 
 export default Input;
